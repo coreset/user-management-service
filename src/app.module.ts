0783a683c0f13b9e5e4f4eb506d98e36e9edddea
samadhi laksahan <samadhivkcom@gmail.com>
2025-04-03 01:07:42 +0530

    add mysql database with typeorm and @nestjs/config for load env files

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,43 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config'; // To load the environment variables
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { DataSource } from 'typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true, // Makes ConfigModule available globally
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => {
+        const dbConfig = {
+          type: 'mysql' as const, // Database type
+          host: configService.get<string>('DB_HOST', 'localhost'),
+          port: +configService.get<number>('DB_PORT', 3306), // Convert string to number
+          username: configService.get<string>('DB_USERNAME', 'root'),
+          password: configService.get<string>('DB_PASSWORD', ''),
+          database: configService.get<string>('DB_DATABASE', 'test'),
+          entities: [__dirname + '/**/*.entity{.ts,.js}'], // Path to your entities
+          synchronize: configService.get('DB_SYNCHRONIZE') === 'true', // Sync database every app startup (development only)
+        };
+
+        const dataSource = new DataSource(dbConfig);
+        try {
+          await dataSource.initialize();
+          console.log(`Database ${dbConfig.database} connected successfully!`);
+        } catch (error) {
+          console.log('Error connecting to the database:', error);
+          throw error;
+        }
+
+        return dbConfig;
+      },
+      inject: [ConfigService],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
